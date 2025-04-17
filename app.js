936b11859744d58a7dc780bc89d2f1d3e00dd7b8
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-17 20:49:29 +0500

    feat: routes

--- app.js
+++ app.js
@@ -1,8 +1,15 @@
 import express from "express";
 import { PORT } from "./config/env.js";
+import userRouter from "./routes/user.routes.js";
+import authRouter from "./routes/auth.routes.js";
+import subscriptionRouter from "./routes/subscription.routes.js";
 
 // initializing the app
 const app = express();
+
+app.use("/api/v1/auth", authRouter);
+app.use("/api/v1/users", userRouter);
+app.use("/api/v1/subscriptions", subscriptionRouter);
 
 // creating first route
 app.get("/", (req, res) => {

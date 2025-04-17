3c63ac9cfeeb91af0828ecfa0a195bcd09730be2
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-17 19:06:35 +0500

    feat: created express server

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log("server running");
+import express from "express";
+
+// initializing the app
+const app = express();
+
+// creating first route
+app.get("/", (req, res) => {
+  res.send(`Welcome to Subscription Tracker API`);
+});
+
+// to make the server listen for requests trying to access specific routes
+app.listen(3000, () => {
+  console.log(`Subscription Tracker API is running on http://localhost:3000`);
+});
+
+export default app;

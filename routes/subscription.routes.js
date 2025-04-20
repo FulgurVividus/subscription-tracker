f7d8579cb1fb2810dc1494590446992fcbf7d4e0
FulgurVividus <murodjon.muzaffarov1@gmail.com>
2025-04-20 21:46:05 +0500

    feat: subscriptions

--- routes/subscription.routes.js
+++ routes/subscription.routes.js
@@ -1,35 +1,44 @@
 import { Router } from "express";
+import authorize from "../middlewares/auth.middleware.js";
+import {
+  createSubscription,
+  getUserSubscriptions,
+} from "../controllers/subscription.controller.js";
 
 const subscriptionRouter = Router();
 
+// Path: /api/v1/subscriptions (GET)
 subscriptionRouter.get("/", (req, res) =>
   res.send({ title: "GET all subscriptions" })
 );
 
+// Path: /api/v1/subscriptions/:id (GET)
 subscriptionRouter.get("/:id", (req, res) =>
   res.send({ title: "GET subscription details" })
 );
 
-subscriptionRouter.post("/", (req, res) =>
-  res.send({ title: "CREATE subscription" })
-);
+// Path: /api/v1/subscriptions (POST)
+subscriptionRouter.post("/", authorize, createSubscription);
 
+// Path: /api/v1/subscriptions/:id (PUT)
 subscriptionRouter.put("/:id", (req, res) =>
   res.send({ title: "UPDATE subscription" })
 );
 
+// Path: /api/v1/subscriptions/:id (DELETE)
 subscriptionRouter.delete("/:id", (req, res) =>
   res.send({ title: "DELETE subscription" })
 );
 
-subscriptionRouter.get("/user/:id", (req, res) =>
-  res.send({ title: "GET all user subscriptions" })
-);
+// Path: /api/v1/subscriptions/user/:id (GET)
+subscriptionRouter.get("/user/:id", authorize, getUserSubscriptions);
 
+// Path: /api/v1/subscriptions/:id/cancel (PUT)
 subscriptionRouter.put("/:id/cancel", (req, res) =>
   res.send({ title: "CANCEL subscription" })
 );
 
+// Path: /api/v1/subscriptions/upcoming-renewals (GET)
 subscriptionRouter.get("/upcoming-renewals", (req, res) =>
   res.send({ title: "GET upcoming renewals" })
 );
